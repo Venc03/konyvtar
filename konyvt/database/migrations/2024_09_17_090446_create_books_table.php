d1c4afb91ee8af8efa777d4c4e564072d76aa0b4
Venc03 <vegi.dani1@gmail.com>
2024-09-17 11:29:44 +0200

    first commit

---
+++ konyvt/database/migrations/2024_09_17_090446_create_books_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\Books;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id('book_id');
+            $table->string('author');
+            $table->string('title');
+            $table->timestamps();
+        });
+        
+        Books::create(['author'=>'Shakespear', 'title'=> 'The shaking of a spear']);
+        Books::create(['author'=>'Roberto', 'title'=> 'Banananana']);
+        Books::create(['author'=>'Bradford', 'title'=> 'Making of a man']);
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('books');
+    }
+};
